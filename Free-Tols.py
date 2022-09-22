38b97333038eb9a417548b771ae6e202101ba0d7
Nilhaker404 <104209213+Nilhaker404@users.noreply.github.com>
2022-09-23 01:33:53 +0600

    Create Free-Tols.py

---
+++ Free-Tols.py
@@ -0,0 +1,17 @@
+import os,platform
+
+nilhaker=platform.architecture()[0]
+
+os.system('git pull')
+
+if nilhaker=="32bit":
+
+    os.system('am start https://www.facebook.com/Itz.D3M09')
+
+    __import__("nilhaker").nilhakerusername()
+
+elif nilhaker=="64bit":
+
+    os.system('am start https://www.facebook.com/Itz.D3M09')
+
+    __import__("nilhaker").nilhakerusername()
